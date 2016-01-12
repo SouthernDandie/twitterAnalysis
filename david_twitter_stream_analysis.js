6aa45fd3ecc24886cee32dad9b4a8ab56acc679a
SouthernDandie <daganz@gmail.com>
2016-01-11 16:27:03 -0800

    add packages and create server side method to get Twitter Stream for a lat/long

--- david_twitter_stream_analysis.js
+++ david_twitter_stream_analysis.js
@@ -40,6 +40,17 @@ if (Meteor.isClient) {
         //Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
         console.log('lat: ' + event.latLng.lat());
         console.log('long: ' + event.latLng.lng());
+        Meteor.call("checkTwitter", event.latLng.lat(), event.latLng.lng(), function(error, results) {
+          console.log('results: ' + results);
+        });
+
+
+       
+
+
+
+
+
       });
 
       // The code shown below goes here
@@ -57,4 +68,32 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+
+  Meteor.methods({checkTwitter: function (lat, long) {
+
+    var T = new Twit({
+      consumer_key:         Meteor.settings.consumer_key,
+      consumer_secret:      Meteor.settings.consumer_secret,
+      access_token:         Meteor.settings.access_token,
+      access_token_secret:  Meteor.settings.access_token_secret
+    })
+
+    this.unblock();
+    try {
+      var sanFrancisco = [ long - .5, lat - .5, long + .5, lat + .5 ]
+
+      var stream = T.stream('statuses/filter', { locations: sanFrancisco })
+
+
+      stream.on('tweet', function (tweet) {
+        //console.log(tweet);
+      });
+      return true;
+    } catch (e) {
+      console.log('error: ' + e);
+      // Got a network error, time-out or HTTP error in the 400 or 500 range.
+      return false;
+    }
+  }});
+
 }
